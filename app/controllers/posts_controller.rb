6259ffa338698ac60668e41c035febcb0f40bc97
activeliang <activeliang@gmail.com>
2017-03-16 22:06:50 +0800

    implement my posts and my groups page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,30 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.fina(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path
+    flash[:alert] = "Post Delete!"
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
