9370039e272d8753e2530cc261c97ca37942ca4f
activeliang <activeliang@gmail.com>
2017-03-16 19:57:13 +0800

    implement CURD

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,59 @@
 class GroupsController < ApplicationController
-
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only:[:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user = current_user
+    if @group.save
+      redirect_to root_path,notice: "Create Success!"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    if @group.update(group_params)
+      redirect_to root_path, notice: "Update Success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    
+    @group.destroy
+    redirect_to root_path, alert: "Group Deleted!"
+  end
+  private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
+
+
+
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+
+  validates :title, presence: true
+
+  belongs_to :user
 end
